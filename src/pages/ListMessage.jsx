88536eb5f6f90693ab097c58b0537f5e8cc3f6f8
BouazziH <bouazzicdi@gmail.com>
2022-08-17 16:34:18 +0200

    ajout de css pour la listes des contacts

--- src/pages/ListMessage.jsx
+++ src/pages/ListMessage.jsx
@@ -2,20 +2,25 @@ import "./ListContacts.css";
 import x from "../data/dbContact.json";
 export default function ListMessage() {
   return (
-    <div>
+    <div className="box">
       <h1>la liste des messages.</h1>
       <ul>
         {x.contacts.map((contact) => (
           <li key={contact.id}>
             <table>
+                <thead>
               <tr>
-                <td>Nom</td>
-                <td>Message</td>
+                <th scope="col">Nom</th>
+                <th scope="col">Message</th>
               </tr>
-              <tr>
+              </thead>
+             <tbody>
+                 <tr>
+                
                 <td>{contact.nom}</td>
                 <td>{contact.message}</td>
               </tr>
+              </tbody>
             </table>
           </li>
         ))}
